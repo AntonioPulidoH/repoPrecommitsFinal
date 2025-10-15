f0a8cd1c55b7e95d970b4f1189a46dd8256313f4
AntonioPulidoH <antoniophjm@gmail.com>
2025-10-15 23:16:22 +0200

    style: format code

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,12 @@
 import pytest
 from app import app as flask_app
 
+
 @pytest.fixture
 def client():
     flask_app.testing = True
     return flask_app.test_client()
+
 
 def test_home_ok(client):
     resp = client.get("/")
