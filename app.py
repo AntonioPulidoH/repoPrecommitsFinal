4b36667731998672a75395fd7a0b7321d9bfdbd0
AntonioPulidoH <antoniophjm@gmail.com>
2025-10-15 21:36:48 +0200

    feat: testing

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return "¡Hola desde Docker! 🚀"
 
+
+# main
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
